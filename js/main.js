680749feabc788b88d1096ad6c2fd3dbed650665
vail <ysywix@gmail.com>
2019-10-15 19:39:37 +0300

    Try to add async to main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,34 @@
 'use strict'
 
-document.addEventListener('DOMContentLoaded', function () {
-  const buttonsContainer = document.querySelector('.buttons-wrap')
-  const fridgeInfoContainer = document.querySelector('.card_size_m:nth-child(8) .card-description')
+const buttonsContainer = document.querySelector('.buttons-wrap')
+const fridgeInfoContainer = document.querySelector('.card_size_m:nth-child(8) .card-description')
 
-  const confirmPurchaseButton = document.querySelector('.buttons-wrap .button_yellow')
-  const purchaseListContainer = document.createElement('div')
-  const purchaseListTitle = document.createElement('p')
-  const purchaseList = document.createElement('ol')
-  const purchaseListItemOne = document.createElement('li')
-  const purchaseListItemTwo = document.createElement('li')
+const confirmPurchaseButton = document.querySelector('.buttons-wrap .button_yellow')
+const purchaseListContainer = document.createElement('div')
+const purchaseListTitle = document.createElement('p')
+const purchaseList = document.createElement('ol')
+const purchaseListItemOne = document.createElement('li')
+const purchaseListItemTwo = document.createElement('li')
 
-  purchaseListContainer.setAttribute('class', 'purchase-list-wrap')
-  purchaseListTitle.setAttribute('class', 'card-description card-description_big description_critical')
-  purchaseListTitle.textContent = 'Список покупок:'
-  purchaseList.setAttribute('class', 'purchase-list')
-  purchaseListItemOne.setAttribute('class', 'purchase-list__item')
-  purchaseListItemOne.textContent = 'Хлеб'
-  purchaseListItemTwo.setAttribute('class', 'purchase-list__item')
-  purchaseListItemTwo.textContent = 'Молоко'
+purchaseListContainer.setAttribute('class', 'purchase-list-wrap')
+purchaseListTitle.setAttribute('class', 'card-description card-description_big description_critical')
+purchaseListTitle.textContent = 'Список покупок:'
+purchaseList.setAttribute('class', 'purchase-list')
+purchaseListItemOne.setAttribute('class', 'purchase-list__item')
+purchaseListItemOne.textContent = 'Хлеб'
+purchaseListItemTwo.setAttribute('class', 'purchase-list__item')
+purchaseListItemTwo.textContent = 'Молоко'
 
-  purchaseListContainer.appendChild(purchaseListTitle)
-  purchaseListContainer.appendChild(purchaseList)
-  purchaseList.appendChild(purchaseListItemOne)
-  purchaseList.appendChild(purchaseListItemTwo)
+purchaseListContainer.appendChild(purchaseListTitle)
+purchaseListContainer.appendChild(purchaseList)
+purchaseList.appendChild(purchaseListItemOne)
+purchaseList.appendChild(purchaseListItemTwo)
 
-  confirmPurchaseButton.onclick = () => {
-    fridgeInfoContainer.replaceWith(purchaseListContainer)
-    buttonsContainer.style.display = 'none'
-  }
+confirmPurchaseButton.onclick = () => {
+  fridgeInfoContainer.replaceWith(purchaseListContainer)
+  buttonsContainer.style.display = 'none'
+}
 
-  document.getElementsByClassName('header-menu__switcher')[0].addEventListener('click', function () {
-    document.getElementsByClassName('header-menu')[0].classList.toggle('header-menu_active')
-  })
-}, !1)
+document.getElementsByClassName('header-menu__switcher')[0].addEventListener('click', function () {
+  document.getElementsByClassName('header-menu')[0].classList.toggle('header-menu_active')
+})
