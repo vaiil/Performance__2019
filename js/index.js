6fc73c41c155e409483baa7f733426a661d1feba
vail <yxllxy@ya.ru>
2019-10-15 12:15:18 +0300

    Async multimedia

--- js/index.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const multimediaScript = document.createElement('script');
-    multimediaScript.setAttribute('src','./js/multimedia.js');
-    
-    document.querySelector('.multimedia').appendChild(multimediaScript);
-});
